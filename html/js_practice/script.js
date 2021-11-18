d4528fbbcd91e636f997ed1985a7924203471b6e
Ken <kennethabreu1@gmail.com>
2021-11-17 21:09:08 -0500

    changed dynamic div to p

--- html/js_practice/script.js
+++ html/js_practice/script.js
@@ -1,4 +1,16 @@
-document.getElementById("button").onclick = function() {
-    document.getElementById("confirm").innerHTML = "Order placed, check email.";
-    document.getElementById("button").style.display = "none";
+document.body.onload = addElement;
+
+function addElement () {
+  // create a new div element
+  const newPara = document.createElement("p");
+
+  // and give it some content
+  const newContent = document.createTextNode("Hi there and greetings!");
+
+  // add the text node to the newly created div
+  newPara.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newPara, currentDiv);
 }
