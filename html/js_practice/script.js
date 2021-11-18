52a42188aa486a0b0c1f09472c4e17eff262d429
Ken <kennethabreu1@gmail.com>
2021-11-18 08:17:51 -0500

    commit

--- html/js_practice/script.js
+++ html/js_practice/script.js
@@ -1,13 +1,14 @@
 document.body.onload = addElement;
+let text = "Sample Text"
 
 function addElement () {
-  // create a new div element
+  // create a new p element
   const newPara = document.createElement("p");
 
   // and give it some content
-  const newContent = document.createTextNode("Hi there and greetings!");
+  const newContent = document.createTextNode(text);
 
-  // add the text node to the newly created div
+  // add the text node to the newly created p
   newPara.appendChild(newContent);
 
   // add the newly created element and its content into the DOM
